62470e5fc4f2b9c7057c262ccb5a221b31628749
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-04-10 21:56:31 +0530

    day 1 not leav9ng

---
+++ Main.java
@@ -0,0 +1,75 @@
+import java.util.*;
+
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         int a = 2;
+//         int b = 5;
+
+//         if(a>b){
+//             System.out.println("a is bigger");
+//         }
+//         else{
+//             System.out.println("b is");
+//         }
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         int x = 445;
+//         if (x % 2 == 0) {
+//             System.out.println("x is even sir");
+//         }
+//         else{
+//             System.out.println("no is odd");
+//         }
+        
+//     }
+// }
+
+
+// find the larrgest of the three no ???
+
+
+// public class Main{
+//     public static void main(String[] args) {
+//         int a = 2;
+//         int b = 5;
+//         int c = 4;
+
+
+//         if(a>b){
+//             System.out.println("a is greater");
+//         }
+//         else if(b>c) {
+//             System.out.println("b is greater");
+//         }
+//         elseif(c>b )
+//     }
+// }
+
+// learining ternary operator
+
+
+// public class Main {
+
+//     public static void main(String[] args) {
+//         int marks = 34;
+//         String reportcard = marks >= 30 ? "pass" : "fail";
+//         System.out.println(reportcard);
+//     }
+// }
+
+
+
+/////////////// ------------ switch statemtne _______;;;;??
+ 
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
