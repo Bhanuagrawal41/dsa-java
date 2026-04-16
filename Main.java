54eccf77e7e1ef00eb5eaad558821ccf485fa86a
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-04-16 22:51:35 +0530

    one more

--- Main.java
+++ Main.java
@@ -285,11 +285,24 @@ public class Main{
 ///--------- checking whether the no is prime or not --------//
 /// 
    public static void main(String[] args) {
-    for(int i = 1; i <=5; i++){
-      for(int j = 1; j <= i; j++){
-            System.out.print("*");
+//     for(int i = 1; i <=5; i++){
+//       for(int j = 1; j <= i; j++){
+//             System.out.print("*");
+//       }
+//       System.out.println();
+//     }
+
+      // for(int i = 1; i<=4; i++){
+      //       for(int j = 1; j <= 4-i+1; j++){
+      //             System.out.print("*");
+      //       }
+      //       System.out.println();
+      // }
+      for(int i = 1; i<=4; i++){
+            for(int j = 1; j <= i; j++){
+                  System.out.print(j);
+            }
+            System.out.println();
       }
-      System.out.println();
-    }
    }
 }
