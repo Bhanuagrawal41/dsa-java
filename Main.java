edcf3c4ddcdfb762dba91127e6f5f488fa7fcd7d
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-04-16 22:32:02 +0530

    working on pattern

--- Main.java
+++ Main.java
@@ -285,9 +285,11 @@ public class Main{
 ///--------- checking whether the no is prime or not --------//
 /// 
    public static void main(String[] args) {
-    for(int i = 5; i<=1; i--){
-          System.out.println("hello");
+    for(int i = 1; i <=5; i++){
+      for(int j = 1; j <= i; j++){
+            System.out.print("*");
+      }
+      System.out.println();
     }
-
    }
 }
