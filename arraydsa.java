6527c8371a108f614c368dd8fbc463f31460835e
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-04-19 21:36:56 +0530

    hello

--- arraydsa.java
+++ arraydsa.java
@@ -1,26 +1,53 @@
 import java.util.*;
 
 public class arraydsa {
-    public static void main(String[] args) {
-        int marks[] = new int[100];
 
-        Scanner sc = new Scanner(System.in);
-
-        marks[0] = sc.nextInt();
-        marks[1] = sc.nextInt();
-        marks[2] = sc.nextInt();
-
-        System.out.println("your marks maths:" + marks[0]);
-        System.out.println("your marks physcis:" + marks[1]);
-        System.out.println("your marks chemistry :" + marks[2]);
-
-        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
-        System.out.println("your percentage is : " + percentage + "%");
-
-        System.out.println(marks.length + " : this is the length of the array");
+    // public static void arrayI(int marks[]){
+    //     for(int i = 0; i < marks.length; i++){
+    //         marks[i] = marks[i] + 1;
+        
+    //     }
+    // }
 
 
-
-    }
+    // public static void main(String[] args) {
+    //    int marks[] = {1, 58, 96};
+    //    arrayI(marks);
+    //    // for printing the marks
+    //    for(int i = 0; i < marks.length; i++){
+    //     System.out.print(marks[i] + " ");
+    //    }
+    //    System.out.println();
+       
+    // }
     
+    //    public static void main(String[] args) {
+    //     int marks[] = {1,2,34};
+          
+    //      for(int i = 0; i < marks.length; i++){
+    //         if (marks[i] == 5) {
+    //             System.out.println("we have finded the required no :" + marks[i]);
+                
+    //         }else{
+    //             System.out.println("no not found");
+    //         }
+    //      }
+    //    }   
+    //--------------------this is the lenear search ----- before the teacher explains to me -////////////
+     //----------now doing it using the functions -----------//
+         
+     public static void lenear(int match[]){
+        
+
+     }
+
+
+
+     public static void main(String[] args) {
+       int match [] = {99, 85 ,69, 2, 10, 56, 89};
+     }
+
+    
+
+
 }
