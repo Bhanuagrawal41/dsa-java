3a23ff7bef92c87abd7d847a9a666b492c4bea02
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-14 17:02:22 +0530

    max length or max no of pair problem

--- greddy.cpp
+++ greddy.cpp
@@ -50,54 +50,92 @@
 //     }
 // };
 
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+//             bool compare(pair<double, int> p1, pair<double, int> p2 ){
+//         return p1.first > p2.first;
+//     }
+
+// int fractionalknapsack(vector<int> val, vector<int> wt, int W){
+//         int n = val.size();
+//         vector<pair<double, int>> ratio(n, make_pair(0.0, 0));
+
+//         for(int i = 0; i<n; i++){
+//             double r = val[i]/(double)wt[i];
+//             ratio[i] = make_pair(r, i);
+//         }
+//         sort(ratio.begin(), ratio.end(), compare);
+
+
+//             int ans = 0;
+
+//         for(int i = 0; i<n; i++){
+//             int idx = ratio[i].second;
+            
+//              if(wt[idx] <= W){
+                
+//                 ans += val[idx];
+//                 W -= wt[idx];
+              
+//              }else{
+//                 ans +=  W*ratio[i].first;
+//                 W = 0;
+//                 break;
+//              }
+
+             
+//         }
+//         return ans;
+// }
+
+
+
+// int main(){
+//     vector<int> val = {60, 100, 120};
+//     vector<int> wt = {10, 20, 30};
+
+//     int W = 50;
+
+//    cout<< fractionalknapsack(val, wt, W);
+// }
+
+
+
+// max length chain of pairs
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-            bool compare(pair<double, int> p1, pair<double, int> p2 ){
-        return p1.first > p2.first;
+bool compare(pair<int , int> p1, pair<int, int> p2){
+    return p1.second < p2.second;
+}
+  
+int Max_Length_Chain(vector<pair<int, int>> chain){
+    sort(chain.begin(), chain.end(), compare);
+    int max_length = chain[0].second - chain[0].first;
+    int last_End = chain[0].second;
+
+    for(int i = 1; i<chain.size(); i++){
+    
+        if(last_End < chain[i].first){
+            max_length += chain[i].second - chain[i].first;
+            last_End = chain[i].second;
+        }
     }
 
-int fractionalknapsack(vector<int> val, vector<int> wt, int W){
-        int n = val.size();
-        vector<pair<double, int>> ratio(n, make_pair(0.0, 0));
+    return max_length;
 
-        for(int i = 0; i<n; i++){
-            double r = val[i]/(double)wt[i];
-            ratio[i] = make_pair(r, i);
-        }
-        sort(ratio.begin(), ratio.end(), compare);
-
-
-            int ans = 0;
-
-        for(int i = 0; i<n; i++){
-            int idx = ratio[i].second;
-            
-             if(wt[idx] <= W){
-                
-                ans += val[idx];
-                W -= wt[idx];
-              
-             }else{
-                ans +=  W*ratio[i].first;
-                W = 0;
-                break;
-             }
-
-             
-        }
-        return ans;
 }
 
 
-
 int main(){
-    vector<int> val = {60, 100, 120};
-    vector<int> wt = {10, 20, 30};
+    vector<pair<int, int>> chain = {{5,24}, {39, 60}, {5, 28}, {27, 40}, {50, 90}};
 
-    int W = 50;
-
-   cout<< fractionalknapsack(val, wt, W);
+   cout << Max_Length_Chain(chain);
 }
