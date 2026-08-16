a13ad8abcded209b573a7a5cac16480f37dc0172
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-16 23:02:52 +0530

    revising some arraay

--- Arraycc.java
+++ Arraycc.java
@@ -101,7 +101,29 @@ public class Arraycc{
 
 //   }
 
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_map<int, int> hash;
+        int n = nums.size();
+        vector<int> ans;
+        
 
+        for(int i = 0;  i < n; i++){
+            hash[nums[i]]++;
+        
+        }
+
+        for(auto it : hash){
+
+            if(it.second > n/3){
+                ans.emplace_back(it.first);
+            }
+        }
+        return ans;
+
+    }
+};
 
   
 
