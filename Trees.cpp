3ae27f3b2e534c7a8b3d3c9de840a61c6959702f
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-17 14:24:32 +0530

    hello

--- Trees.cpp
+++ Trees.cpp
@@ -7,22 +7,39 @@ class TreeNode{
     int data;
     TreeNode* left;
     TreeNode* right;
-    TreeNode(int val){
-        data = val;
+    TreeNode(int data){
+        this->data = data;
         left = NULL;
         right = NULL;
     }
 
 };
 
-class Tree{
 
-};
+static int idx = -1;
 
+TreeNode* buildTree(vector<int> nodes){
+       idx++;
+       if(nodes[idx] == -1){
+        return NULL;
+       }
+
+
+
+       TreeNode* currNode = new TreeNode(nodes[idx]);
+       currNode->left = buildTree(nodes);
+       currNode->right = buildTree(nodes);
+
+         cout << currNode->data << endl;
+       return currNode;
+}
 
 
 
 
 int main(){
+      
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+     cout << buildTree(nodes);
 
 }
