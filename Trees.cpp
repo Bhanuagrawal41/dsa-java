3401adc09c191103ff82e67a01d0b7a56baf5f30
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-17 12:17:56 +0530

    starting and revising trees today

---
+++ Trees.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class TreeNode{
+    public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+
+};
+
+class Tree{
+
+};
+
+
+
+
+
+int main(){
+
+}
