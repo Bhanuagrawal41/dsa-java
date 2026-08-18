32dc692edfd47f6cdce4bf40b30ddcde59ed11ce
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-18 21:59:47 +0530

    level order zig zag traversal:)

--- Trees.cpp
+++ Trees.cpp
@@ -102,3 +102,52 @@ int main(){
 
 
 }
+
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+
+        if (root == NULL)
+            return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        bool flag = true;
+
+        while (!q.empty()) {
+            int n = q.size();
+
+            vector<int> v(n);
+
+            for (int i = 0; i < n; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+
+                int idx;
+
+                if (flag) {
+                    idx = i;
+                } else {
+                    idx = n - 1 - i;
+                }
+
+                v[idx] = temp->val;
+
+                if (temp->left)
+                    q.push(temp->left);
+
+                if (temp->right)
+                    q.push(temp->right);
+            }
+
+            ans.push_back(v);
+
+            flag = !flag;
+        }
+
+        return ans;
+    }
+};
