345be598bad4528dee01bd2998132bac9c11188f
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-21 21:59:08 +0530

    leet coe 110 kindda twisted easy prob:)

--- Trees.cpp
+++ Trees.cpp
@@ -111,6 +111,16 @@ int count(TreeNode* root){
 
    return (right +  left)+ 1;
 }
+int Sum_of_Nodes(TreeNode* root){
+    if(root == nullptr){
+        return 0;
+    }
+
+    int right = Sum_of_Nodes(root -> left);
+    int left = Sum_of_Nodes(root -> right);
+
+    return (right + left + root->data);
+}
 
 
 
@@ -176,3 +186,44 @@ int main(){
 //         return ans;
 //     }
 // };
+
+// this easy leet code is scary man
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+  
+        return dfsHeight(root) != -1;
+    }
+
+    int dfsHeight(TreeNode* root){
+        if(root == NULL){
+            return 0;
+        }
+
+        int left = dfsHeight(root->left);
+         if(left == -1) return -1;
+
+        int right = dfsHeight(root->right);
+        if(right == -1) return -1;
+
+        if(abs(left - right) > 1){
+            return -1;
+        }
+
+        return max(left, right) + 1;
+    }
+
+
+};
