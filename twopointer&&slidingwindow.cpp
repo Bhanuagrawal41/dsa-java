90694f47dbfc34d8fb78b73586ec7e9687ceeee3
Bhanuagrawal41 <bhanuagrawal25641@gmail.com>
2026-08-25 23:35:49 +0530

    two pointer sliding window and hash map problem leet code no 904

--- twopointer&&slidingwindow.cpp
+++ twopointer&&slidingwindow.cpp
@@ -1,27 +1,68 @@
-class Solution {
-public:
-    int longestOnes(vector<int>& nums, int k) {
-        int count = 0;
-        int left = 0;
-         int count0 = 0;
+// class Solution {
+// public:
+//     int longestOnes(vector<int>& nums, int k) {
+//         int count = 0;
+//         int left = 0;
+//          int count0 = 0;
         
 
-          for(int i = 0; i < nums.size(); i++){
+//           for(int i = 0; i < nums.size(); i++){
             
            
-            if(nums[i] == 0){
-                count0++;              
-            }
-            while(count0 > k){
-                if(nums[left] == 0){
-                  count0--;
-                }
-                left++;
-            }
-            count = max(count, i - left + 1);
+//             if(nums[i] == 0){
+//                 count0++;              
+//             }
+//             while(count0 > k){
+//                 if(nums[left] == 0){
+//                   count0--;
+//                 }
+//                 left++;
+//             }
+//             count = max(count, i - left + 1);
 
-          }
-          return count;
+//           }
+//           return count;
         
+//     }
+// };
+
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        
+        unordered_map<int, int> hash;
+        int first = 0;
+          int count = 0;
+          int ans = 0;
+        for(int i = 0; i < fruits.size(); i++){
+            if(hash[fruits[i]] == 0){
+                count++;
+            }
+            hash[fruits[i]]++;
+            int var = fruits[first];
+            
+             
+            while(count > 2){
+                hash[fruits[first]]--;
+                if(hash[fruits[first]] == 0){
+                    count--;
+                }
+                first++;
+            
+               
+                
+            }
+            ans = max(ans, i - first + 1);
+          
+           
+
+           
+        }
+
+        return ans;
+   
+          
+
     }
 };
